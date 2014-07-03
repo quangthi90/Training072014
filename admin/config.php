e7929060437dee2a5f70231bc44a1b69c03fcf02
Bommer <quangthi_90@yahoo.com.vn>
2014-07-03 13:36:11 +0700

    - Move code to Root folder
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/Training/admin/');
+define('HTTP_CATALOG', 'http://localhost/Training/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/Training/admin/');
+define('HTTPS_CATALOG', 'http://localhost/Training/');
+
+// DIR
+define('DIR_APPLICATION', 'F:\Program\xampp\htdocs\Training/admin/');
+define('DIR_SYSTEM', 'F:\Program\xampp\htdocs\Training/system/');
+define('DIR_DATABASE', 'F:\Program\xampp\htdocs\Training/system/database/');
+define('DIR_LANGUAGE', 'F:\Program\xampp\htdocs\Training/admin/language/');
+define('DIR_TEMPLATE', 'F:\Program\xampp\htdocs\Training/admin/view/template/');
+define('DIR_CONFIG', 'F:\Program\xampp\htdocs\Training/system/config/');
+define('DIR_IMAGE', 'F:\Program\xampp\htdocs\Training/image/');
+define('DIR_CACHE', 'F:\Program\xampp\htdocs\Training/system/cache/');
+define('DIR_DOWNLOAD', 'F:\Program\xampp\htdocs\Training/download/');
+define('DIR_LOGS', 'F:\Program\xampp\htdocs\Training/system/logs/');
+define('DIR_CATALOG', 'F:\Program\xampp\htdocs\Training/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'Training');
+define('DB_PREFIX', 'oc_');
+?>
