8f644db8d292e7bdf6687bc3f24d8aec3a05dde6
tuyetphale993 <tuyetphale993@gmail.com>
2014-07-25 15:05:22 +0700

    - SQl Topic 5
    Signed-off-by: tuyetphale993 <tuyetphale993@gmail.com>

---
+++ topic5.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.6
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 25, 2014 at 10:04 AM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `topic5`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `account`
+--
+
+CREATE TABLE IF NOT EXISTS `account` (
+  `Username` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `Password` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`Username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `account`
+--
+
+INSERT INTO `account` (`Username`, `Password`) VALUES
+('thuy', '12345'),
+('trang', '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE IF NOT EXISTS `comment` (
+  `IDPost` int(11) NOT NULL,
+  `UserCmt` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `Date` datetime NOT NULL,
+  `Content` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`IDPost`,`Date`),
+  KEY `User` (`UserCmt`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`IDPost`, `UserCmt`, `Date`, `Content`) VALUES
+(1, 'trang', '2014-07-24 00:00:00', 'Sao cung dc nhi?!'),
+(1, 'thuy', '2014-07-24 09:00:00', 'Khong dc');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+CREATE TABLE IF NOT EXISTS `post` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Date` datetime NOT NULL,
+  `Content` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `UserPost` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `fk_post_account` (`UserPost`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `post`
+--
+
+INSERT INTO `post` (`ID`, `Date`, `Content`, `UserPost`) VALUES
+(1, '2014-07-22 00:00:00', 'Sao cung dc nhi?!', 'thuy');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`IDPost`) REFERENCES `post` (`ID`),
+  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`UserCmt`) REFERENCES `account` (`Username`);
+
+--
+-- Constraints for table `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `fk_post_account` FOREIGN KEY (`UserPost`) REFERENCES `account` (`Username`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
