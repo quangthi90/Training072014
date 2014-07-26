e5c1df42911c2c25e11e3e3c72f7fb5205dbf9aa
tuyetphale993 <tuyetphale993@gmail.com>
2014-07-26 15:22:13 +0700

    - update
    Signed-off-by: tuyetphale993 <tuyetphale993@gmail.com>

--- Topic5/index.php
+++ Topic5/index.php
@@ -39,6 +39,7 @@
 		</div>
 		<div class="row">
 			<div class="col-sm-2 col-md-2 col-lg-2">
+				<a href="index.php" class="btn btn-success pager center-block">Home Page</a>
 				<nav class="navbar navbar-default" role="navigation" style="background-color:#ffcc00; padding-bottom: 10px">
 					<!-- Brand and toggle get grouped for better mobile display -->
 					<h5 class="text-danger" style="margin-left: 20px">List user</h5>
@@ -59,14 +60,6 @@
 			</div>
 			<div class="col-sm-8 col-md-8 col-lg-8">
 				<ul class="list-group">
-				  <!--li class="list-group-item">
-				  	<h4><strong style="float:left">User 1</strong>
-				  		<h3 class="text-center">Title</h3>
-				  	</h4>
-				  	<p>Content Post</p>
-				  	<h5><small>Date post</small></h5>
-				  </li-->
-				
 				 <li class="list-group-item">
 			 		<form action="php/poststatus.php" method="get"> 
 			 			<textarea style="width:100%; border-radius: 5px; " row = "3" id = "status" name="status" placeholder = " Bạn đang nghĩ gì?" ></textarea>
