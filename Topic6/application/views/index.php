65d27d67de0ad43842b22beff2a35d8f9b28acbe
June Huynh <linhphuong109@gmail.com>
2014-07-28 10:09:46 +0700

    Topic 6:

---
+++ Topic6/application/views/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Topic6</title>
+	<link rel="stylesheet" type="text/css" href="template/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="template/css/bootstrap-theme.min.css">
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+				<img src="img/icon.png" class="img-responsive" alt="Responsive image">
+			</div>
+		</div>
+	</div>
+</body>
+</html>
