647e731345e597493a75190b1ff97973a55f9b8b
tuyetphale993 <tuyetphale993@gmail.com>
2014-07-28 23:37:15 +0700

    - update
    Signed-off-by: tuyetphale993 <tuyetphale993@gmail.com>

--- Topic6/application/views/content.php
+++ Topic6/application/views/content.php
@@ -1,4 +1,4 @@
-<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 bg-info">
+<div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">
 				<!-- conntent -->
 				<?php include "/template/add_post.php"; ?>
 			</div>
