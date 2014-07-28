0e2de583bdefb2d564862ba65b0eac45ad3b1136
tuyetphale993 <tuyetphale993@gmail.com>
2014-07-28 16:27:22 +0700

    - template
    Signed-off-by: tuyetphale993 <tuyetphale993@gmail.com>

---
+++ Topic6/application/views/template/add_comment.php
@@ -0,0 +1,14 @@
+<?php
+$str_echo="
+	<div class='row'>
+		<div class='col-xs-2 col-sm-2 col-md-2 col-lg-2 '>
+			<img src='./../img/user1.jpg' class='img-responsive pull-right' style='width: 80px' />	
+		</div>
+
+		<textarea rows='3' placeholder='How do you feel?' class='col-xs-8 col-sm-8 col-md-8 col-lg-8'></textarea>	
+		<div class='col-xs-2 col-sm-2 col-md-2 col-lg-2'>
+			<button type='button' class='btn btn-primary pull-left' style='padding: 5px 20px'>Post</button>
+		</div>
+	</div>"
+echo $str_echo;
+?>
