4cc01b88fd4ede0996221199ff905541b903cf54
Bommer <quangthi_90@yahoo.com.vn>
2014-07-30 00:30:11 +0700

    - Update load module for header  - Change template file location
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

--- Topic6/application/controllers/common/header.php
+++ Topic6/application/controllers/common/header.php
@@ -1,45 +1,20 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
+<?php
+/**
+ * Common Header - Show Header for template
+ * @author: Bommer<lqthi.khtn@gmail.com>
+ * @param: none
+ * @return: template header
+ */
 class Header extends CI_Controller {
 	public function index()
 	{
-		// Load helper
-		$this->load->helper("url");
-		$sResgisterLink = site_url( array('account', 'account', 'register') );
+		// load template
+		$this->load->view('common/header.php');
 
-		$this->load->view('template/header.php');
-
-		$this->load->library('../controllers/common/header');
-		$this->header->index();
-		
-		// load base layout
-		// $this->load->library('../controllers/common/header');
-		// $this->header->index();
-		// $this->load->view('template/header.php');
-		// $this->load->view('template/left.php');
-		// $this->load->view('template/right.php');
-		// $this->load->view('template/header.php');
-		
-		/*$this->load->view('template/header.php');
-		$this->load->view('template/account/signin_view.php', array('sResgisterLink' => $sResgisterLink));
-
-		// GET LIST USER
-		$users = array();
-		$this->load->model("user");
-		$users = $this->user->getUsers();
-		foreach ($users as $key => $user) {
-			$users[$key]['wall_link'] = site_url(array('homepage', 'wall', $user['username']));
-		}
-		$this->load->view('template/list_user.php', array( 'users' => $users));
-		
-		$this->load->model("ListPost");
-		$data['result'] = $this->ListPost->listpost();
-		$this->load->view("viewlistpost", $data);
-		$this->load->view('template/footer.php');*/
-	}
-	public function wallpostdelete($postId){
-		$this->load->model("Wallpost");
-		$this->Wallpost->deletepost($postId);
+		// Load modules
+		include_once(APPPATH.'controllers/module/login.php');
+    	$clsLogin = new Login();
+		$clsLogin->index();
 	}
 }
 ?>
