9d3505e82653aba9536094343975cfc6c15c8467
Bommer <quangthi_90@yahoo.com.vn>
2014-07-29 23:50:04 +0700

    - Remove function construct in controller Homepage (don't need)
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

--- Topic6/application/controllers/Homepage.php
+++ Topic6/application/controllers/Homepage.php
@@ -1,12 +1,9 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class HomePage extends CI_Controller {
-	public function __construct(){
-        parent::__construct();
-         $this->load->helper("url");
-    }
 	public function index()
 	{
+		$this->load->helper("url");
 		$sResgisterLink = site_url( array('account', 'account', 'register') );
 		
 		$this->load->view('template/header.php');
