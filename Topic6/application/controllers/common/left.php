cabe280f2264965028da6d1e426103909839dc2d
Bommer <quangthi_90@yahoo.com.vn>
2014-07-30 01:03:28 +0700

    - Remove temp code
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

--- Topic6/application/controllers/common/left.php
+++ Topic6/application/controllers/common/left.php
@@ -1,4 +1,4 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
 class Left extends CI_Controller {
 	public function index()
@@ -7,35 +7,6 @@ class Left extends CI_Controller {
 		include_once(APPPATH.'controllers/module/listAllUsers.php');
     	$clsListAllUsers = new ListAllUsers();
 		$clsListAllUsers->index();
-
-		// load base layout
-		// $this->load->library('../controllers/common/header');
-		// $this->header->index();
-		// $this->load->view('template/header.php');
-		// $this->load->view('template/left.php');
-		// $this->load->view('template/right.php');
-		// $this->load->view('template/header.php');
-		
-		/*$this->load->view('template/header.php');
-		$this->load->view('template/account/signin_view.php', array('sResgisterLink' => $sResgisterLink));
-
-		// GET LIST USER
-		$users = array();
-		$this->load->model("user");
-		$users = $this->user->getUsers();
-		foreach ($users as $key => $user) {
-			$users[$key]['wall_link'] = site_url(array('homepage', 'wall', $user['username']));
-		}
-		$this->load->view('template/list_user.php', array( 'users' => $users));
-		
-		$this->load->model("ListPost");
-		$data['result'] = $this->ListPost->listpost();
-		$this->load->view("viewlistpost", $data);
-		$this->load->view('template/footer.php');*/
-	}
-	public function wallpostdelete($postId){
-		$this->load->model("Wallpost");
-		$this->Wallpost->deletepost($postId);
 	}
 }
 ?>
