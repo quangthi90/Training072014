b860d4e8eaaaac0ab71c7d193386acf4e08f1747
nguyennga10 <nguyennga.khtn10@gmail.com>
2014-07-29 16:05:16 +0700

    update

--- Topic6/application/models/wallpost.php
+++ Topic6/application/models/wallpost.php
@@ -12,13 +12,9 @@
 			$this->load->database();
 		}
 		public function listpost(){
-			/*$sql = "select * from `post` where `userWall` =  '$user'";
-			$query = $this->db->query($sql);*/
-			$user = $_REQUEST["user"];
-		    $this->db->select("postId, postAuthor, userWall,postTitle, postContent, postCreate,postUpdate");
+		    $this->db->select("postId,postAuthor,userWall,postTitle,postContent,postCreate,postUpdate");
 	        $this->db->where("postWall","$user");
 	        $query=$this->db->get("post");
-			
 	        return $query->result_array();
 		}
 	}
