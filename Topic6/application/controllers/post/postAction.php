7096b97eee5e467f4f4307d8a368d78c656c0a2e
Bommer <quangthi_90@yahoo.com.vn>
2014-07-30 22:46:02 +0700

    - Fix error controller same name with model (Post)
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

--- Topic6/application/controllers/post/postAction.php
+++ Topic6/application/controllers/post/postAction.php
@@ -5,21 +5,21 @@
  * @param: none
  * @return: postId to delete
  */
-class post extends CI_Controller
+class PostAction extends CI_Controller
 {
 	
 	public function delete($postId)
 	{
 		// get post from DB
-		$this->load->model("mpost");
+		$this->load->model("post");
 
 
 		//echo '<pre>';var_dump($postId);exit();
 		// GET USER USERNAME
 		$aUser = $this->session->userdata('username');
-		$aPosts = $this->mpost->getPosts( array('user_wall' => $aUser) );
+		$aPosts = $this->post->getPosts( array('user_wall' => $aUser) );
 		// CALL MODEL: FUNCTION DELETE POST
-		$this->mpost->deletePost($postId, $aUser);
+		$this->post->deletePost($postId, $aUser);
 
 		$this->load->view( "module/listPosts", array('aPosts' => $aPosts) );
 		// REDIRECT TO HOME PAGE
