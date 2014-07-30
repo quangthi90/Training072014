16c00ba311694dcc943ad90f6183f115c5992d55
nguyennga10 <nguyennga.khtn10@gmail.com>
2014-07-30 22:16:44 +0700

    change name model/post.php => model/mpost

---
+++ Topic6/application/models/mpost.php
@@ -0,0 +1,28 @@
+<?php
+	class MPost extends CI_Model
+	{
+		public function getPosts( $aData = array() ) {
+			$sQuery = "SELECT postId, postAuthor, userWall, postTitle, postContent, postCreate, postUpdate FROM Post";
+
+			if ( !empty($aData['user_wall']) ){
+				$sQuery .= " WHERE userWall = '" . $aData['user_wall'] . "'";
+			}
+
+			return $this->db->query($sQuery)->result_array();
+		}
+
+		// FUNCTION DELETE POST
+		public function deletePost($postId, $aUser){
+			// KIEM TRA USER CO DC PHEP DELETE POST NAY HAY KO
+			if ($aUser == $this->db->query("SELECT userWall FROM Post WHERE postId = '$postId'")) {
+				$sql = "DELETE Post WHERE postId = '$postId' AND userWall = '$aUser' OR postAuthor = '$aUser'";
+				$this->db->query($sql);
+			}
+			else{
+				echo "You might not delete";
+			}
+			// DELETE POST
+			
+		}
+	}
+?>
