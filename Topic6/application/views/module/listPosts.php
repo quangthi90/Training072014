3be923650fcca11f48890b57b30fa816a690a18a
tuyetphale993 <tuyetphale993@gmail.com>
2014-07-30 23:32:48 +0700

    - fix list post
    Signed-off-by: tuyetphale993 <tuyetphale993@gmail.com>

--- Topic6/application/views/module/listPosts.php
+++ Topic6/application/views/module/listPosts.php
@@ -9,10 +9,12 @@
 				<small><?php echo $aPost["postCreate"]; ?></small></h5>
 				<input type="hidden" value="<?php echo $aPost['postId']; ?>">
 			    <h3 class="text-uppercase"><strong><?php echo $aPost["postTitle"]; ?></strong></h3>
-			    <a href="postdetail.php" class="pull-right">See more</a><br/>
-			    <a href="<?php echo $aPost['wall_link_delete']; ?>" class="pull-right">Delete</a><br/>
-			    <a href="<?php echo $aPost['wall_link_edit']; ?>" class="pull-right">Edit</a>
 			    <p><?php echo $aPost["postContent"]; ?></p>
+			    <div style="margin-bottom: 10px">
+			    	<a href="postdetail.php" class="pull-right">See more</a><br/>
+				    <a href="<?php echo $aPost['wall_link_delete']; ?>" class="pull-right">Delete</a><br/>
+			    </div>
+			    
 		    </div>
 		<?php } ?>
 <!-- </div> -->
